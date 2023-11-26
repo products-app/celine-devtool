f9231c4baaa0a8fc20d78b41f9f93de94b325eaa
leticiabernardo <leticiaellenbernardo@gmail.com>
2023-11-25 21:35:19 -0300

    feat: create the first page

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { HomePage } from "./HomePage";
+// import MainLayout from '@/components/MainLayout'
+// import { Provider } from 'react-redux'
+// import { store } from '@/store'
+
+export default function Page() {
+  return (
+    // <Provider store={store}>
+      <HomePage />
+    // </Provider>
+  )
+  
+  ;
+}
